8ce1ec62c0711faf0f18d489f78e084a133e24b7
Mohammad Rasel <mohammadrasel1221@gmail.com>
2020-11-26 02:38:35 +0600

    add unit test for movie service

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,51 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'test movie',
+        year: 2020,
+        genres: ['test'],
+      });
+      const result = service.getOne(1);
+      expect(result).toBeDefined();
+      expect(result.title).toEqual('test movie');
+    });
+    it('should throw not found exception.', () => {
+      try {
+        service.getOne(1000);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should remove a movie', () => {
+      service.create({
+        title: 'test movie',
+        year: 2020,
+        genres: ['test'],
+      });
+      service.deleteOne(1);
+      expect(service.getAll().length).toEqual(0);
+    });
+    it('should throw not found exception.', () => {
+      try {
+        service.deleteOne(1000);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
